d5850ad90043063a26cbd9ad5536767fdce63bdd
Madan2468 <madanjha2468@gmail.com>
2024-09-09 22:01:44 +0530

    changed header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,13 +6,7 @@ export default function Header() {
     <header className="shadow sticky z-50 top-0">
       <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
-          <Link to="/" className="flex items-center">
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5vv4NcL5GCumDdgzdK1EDtwwJTARwoJG5Dg&s"
-              className="mr-3 h-12"
-              alt="Logo"
-            />
-          </Link>
+          <Link to="/" className="flex items-center"></Link>
           <div className="flex items-center lg:order-2">
             <a
               href="https://docs.google.com/forms/d/1ZVpEFyy7zY0QWEiotySAYDdS4ral70JkAg73nQpcLMw/edit"
